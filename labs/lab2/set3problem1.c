4f10b95ed933344f25484f2065ef9fd77e69c540
sfulle14 <steven.fuller96@gmail.com>
2023-05-28 20:50:25 -0500

    set3 problem1 works

--- labs/lab2/set3problem1.c
+++ labs/lab2/set3problem1.c
@@ -2,6 +2,25 @@
 
 int main() {
    
-   
+   int num=0;
+   int sum=0;
+   int count=0;
+   float avg=0;
+
+   do{
+      printf("Enter an integer: ");
+      scanf("%d", &num);
+
+      if(num %2 ==0 && num != 0){
+         sum += num;
+         count++;
+      }
+
+   }while(num != 0);
+
+   avg = sum / count;
+
+   printf("The average of the even numbers is: %f", avg);
+
    return 0;
 }
