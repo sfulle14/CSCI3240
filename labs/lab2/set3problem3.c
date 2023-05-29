db04b8d0b04eb20f3362541328602fb980dd9db8
sfulle14 <steven.fuller96@gmail.com>
2023-05-28 21:07:45 -0500

    set3 problem3 is working

--- labs/lab2/set3problem3.c
+++ labs/lab2/set3problem3.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main() {
-   
+   int numTerms;
+   int addTerm=1;
+   int count=1;
+
+   printf("Enter the number of terms: ");
+   scanf("%d", &numTerms);
+
+   for(int i=1; i<numTerms; i++){
+      count += addTerm;
+      addTerm+=2;
+   }
+   printf("The nth term is: %d", count);
    
    return 0;
 }
