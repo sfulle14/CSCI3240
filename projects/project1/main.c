5571bc39615b195e2db9ffa0e2ae86e42939263c
sfulle14 <steven.fuller96@gmail.com>
2023-05-29 08:55:41 -0500

    start project1 still, not working

--- projects/project1/main.c
+++ projects/project1/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
 int main() {
-   
-   
+   //declare variables
+   char file[1000]; //this is an array of char since there is no string in C.
+   char letter;
+
+   //get the file to parce from the user.
+   scanf("%c", &file[1000]);
+
+   FILE *in_file = fopen(file, "r");
+
+   while(fscanf(in_file, "%c", &letter)){
+      printf("%2x", letter);
+   }
+
+   /*
+   //print out the hex of the file.
+   for(int i=0; i<sizeof file; i++){
+      printf("%2x", file[i]);
+   }
+   */
+
+   fclose(in_file);
    return 0;
 }
