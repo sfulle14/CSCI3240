34b64b35eb07447df9a4179621775de32915f2c2
sfulle14 <steven.fuller96@gmail.com>
2023-05-28 20:59:21 -0500

    set3 problem2 works

--- labs/lab2/set3problem2.c
+++ labs/lab2/set3problem2.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main() {
-   
+   int num=0;
+   int sum=0;
+
+   printf("Enter an integer: ");
+   scanf("%d", &num);
+
+   for(int i=1; i < num; i++){
+      sum +=i;
+      printf("%d\n", sum);
+      if(sum > num){
+         printf("%d is not triangular", num);
+         break;
+      }
+      if(sum==num){
+         printf("%d is triangular", num);
+         break;
+      }
+   }
+
    
    return 0;
 }
