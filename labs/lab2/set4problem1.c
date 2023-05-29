d9913a4bb1d49d48fd16f61e9225f9b456d685b8
sfulle14 <steven.fuller96@gmail.com>
2023-05-28 21:26:30 -0500

    set4 problem1 is working

--- labs/lab2/set4problem1.c
+++ labs/lab2/set4problem1.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+int factorial(int num);
+
 int main() {
+   //declare variables
+   int num;
+   int fact;
+
+   //get number from user
+   printf("Enter an integer: ");
+   scanf("%d", &num);
    
+   fact = factorial(num);
+
+   printf("The factorial of %d is: %d", num, fact);
    
    return 0;
+}
+
+//function to find the factorial of a user provided number
+int factorial(int num){
+   int sum=1;
+   for(int i=1; i<=num; i++){
+      sum *= i;
+   }
+
+   return sum;
 }
