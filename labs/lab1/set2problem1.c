12806fda0ceb48e78b2941a2f0db966d36a50776
sfulle14 <steven.fuller96@gmail.com>
2023-05-28 20:36:33 -0500

    testing set2 all problems work

--- labs/lab1/set2problem1.c
+++ labs/lab1/set2problem1.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 
-
 int main() {
+
+   int n;
+
+   printf("Enter an integer: ");
+   scanf("%d", &n);
+
+   switch (n%2)
+   {
+   case 0:
+      printf("%d is an even number.\n", n);
+      break;
+   
+   default:
+      printf("%d is an odd number.\n", n);
+      break;
+   }
 
    return 0;
 }
--- labs/lab1/set2problem2.c
+++ labs/lab1/set2problem2.c
@@ -4,5 +4,24 @@
 
 int main() {
 
+   int salary;
+   int taxes;
+
+   printf("Enter salary: ");
+   scanf("%d", &salary);
+   
+
+   if(salary < 100000){
+      printf("Tax amount is: 0\n");
+   }
+   else if(salary < 175000){
+      taxes = salary * 0.15;
+      printf("Tax amount is: %d\n", taxes);
+   }
+   else{
+      taxes = salary * 0.25;
+      printf("Tax amount is: %d\n", taxes);
+   }
+
    return 0;
 }
