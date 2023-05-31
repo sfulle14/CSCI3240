ccea48630062b946ebc5a2c94ccad1e7f37f0be4
sfulle14 <steven.fuller96@gmail.com>
2023-05-31 08:50:41 -0500

    added row counter in hex and 2 char grouping

--- projects/project1/main.c
+++ projects/project1/main.c
@@ -5,25 +5,36 @@ int main(int argc, char *argv[]) {
    //declare variables
    char file[1000]; //this will hold the file name
    char letter;
-   int counter =0;
+   int counter=0;
    char dot = '.';
+   int line=0;
 
-   //get the file to parce from the user.
-   //scanf("%c", &file[1000]);
-   //printf("File loaded\n");
+
 
    //open file in command line
    FILE *inFile = fopen(argv[1], "r");
 
-   while(fscanf(inFile, "%c", &letter)){
+   //loops throught the file while it reads a char from the file and it has not reached the end of the file 
+   while(fscanf(inFile, "%c", &letter) && !feof(inFile)){
+      //enters a new line every 16 loops
       if(counter==16){
+         line++;
          printf("\n");
          counter=0;
       }
+      //if if is the start of a new line print the hex of the line number
+      if(counter==0){
+         printf("%07x0: ", line);
+      }
+      if(counter%2==0){
+         printf(" ");
+      }
+      //print out the hex of the value read in
       if(isprint(letter)){
          printf("%x", letter);
          counter++;
       }
+      //if the value read in is unprintable print the hex of "."
       else{
          printf("%x", dot);
          counter++;
