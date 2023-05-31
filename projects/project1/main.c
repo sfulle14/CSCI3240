2b0a43f689c7ebdcd2eb1fc2de5cafef2a791a7c
sfulle14 <steven.fuller96@gmail.com>
2023-05-31 09:29:29 -0500

    working! needs formating edits

--- projects/project1/main.c
+++ projects/project1/main.c
@@ -3,28 +3,28 @@
 
 int main(int argc, char *argv[]) {
    //declare variables
-   char file[1000]; //this will hold the file name
    char letter;
    int counter=0;
-   char dot = '.';
-   int line=0;
+   int row=0;
+   char line[16];
 
 
 
    //open file in command line
-   FILE *inFile = fopen(argv[1], "r");
+   FILE *inFile = fopen(argv[1], "r+");
 
    //loops throught the file while it reads a char from the file and it has not reached the end of the file 
    while(fscanf(inFile, "%c", &letter) && !feof(inFile)){
       //enters a new line every 16 loops
       if(counter==16){
-         line++;
+         row++;
+         printf("  %s",line);
          printf("\n");
          counter=0;
       }
       //if if is the start of a new line print the hex of the line number
       if(counter==0){
-         printf("%07x0: ", line);
+         printf("%07x0: ", row);
       }
       if(counter%2==0){
          printf(" ");
@@ -32,23 +32,25 @@ int main(int argc, char *argv[]) {
       //print out the hex of the value read in
       if(isprint(letter)){
          printf("%x", letter);
+         line[counter] = letter;
          counter++;
       }
       //if the value read in is unprintable print the hex of "."
       else{
-         printf("%x", dot);
+         printf("%x", '.');
+         line[counter] = '.';
          counter++;
       }
-      
    }
-
-   /*
-   //print out the hex of the file.
-   for(int i=0; i<sizeof file; i++){
-      printf("%2x", file[i]);
+   //sets all the extra spaces to ' '
+   for(int i=counter; i<16;i++){
+      line[i]=' ';
    }
-   */
+   //prints out the last line
+   printf("  %32s", line);
 
+   //closes the file
    fclose(inFile);
+
    return 0;
 }
