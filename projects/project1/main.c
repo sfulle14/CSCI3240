1a6e09fd735cb5510a85ba3ace51ecd2bb8911a6
sfulle14 <steven.fuller96@gmail.com>
2023-05-31 07:58:16 -0500

    file opens on command line

--- projects/project1/main.c
+++ projects/project1/main.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main() {
+int main(int argc, char *argv[]) {
    //declare variables
-   char file[1000]; //this is an array of char since there is no string in C.
+   char file[1000]; //this will hold the file name
    char letter;
+   int counter =0;
+   char dot = '.';
+
+   printf("Variables created\n");
 
    //get the file to parce from the user.
-   scanf("%c", &file[1000]);
+   //scanf("%c", &file[1000]);
+   //printf("File loaded\n");
 
-   FILE *in_file = fopen(file, "r");
+   FILE *inFile = fopen(argv[1], "r");
+   printf("File opened\n");
 
-   while(fscanf(in_file, "%c", &letter)){
-      printf("%2x", letter);
+/*
+   while(fscanf(inFile, "%c", &letter)){
+      printf("While loop entered\n");
+      if(counter==16){
+         printf("\n");
+         counter=0;
+      }
+      if(isprint(letter)){
+         printf("%x", letter);
+         counter++;
+      }
+      else{
+         printf("%x", dot);
+         counter++;
+      }
+      
    }
 
-   /*
+   
    //print out the hex of the file.
    for(int i=0; i<sizeof file; i++){
       printf("%2x", file[i]);
    }
-   */
-
-   fclose(in_file);
+   
+*/
+   fclose(inFile);
    return 0;
 }
