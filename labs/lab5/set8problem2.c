a05695957d2542e1c90ccfbda1726872c5b23401
sfulle14 <steven.fuller96@gmail.com>
2023-06-15 12:35:07 -0500

    start set8problem2

--- labs/lab5/set8problem2.c
+++ labs/lab5/set8problem2.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+#define PLAYERNUM 5
+
+struct batter{
+    char* name;
+    double battingAvg;
+    int runs;
+    int dismissals;
+};
 
 int main() {
+    struct batter players[PLAYERNUM];
+
+    for(int i=0; i<PLAYERNUM; i++){
+        printf("Enter batter name: ");
+        scanf("%s", players[i].name);
+
+        printf("Enter runs: ");
+        scanf("%d", &players[i].runs);
+
+        printf("Enter dismissals: ");
+        scanf("%d", &players[i].dismissals);
+
+        players[i].battingAvg = (double)players[i].runs / (double)players[i].dismissals;
+    }
 
     return 0;
 }
