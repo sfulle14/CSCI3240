1392be4b16b8c61c58b0bfcd88435bae2da8cc9d
sfulle14 <steven.fuller96@gmail.com>
2023-06-15 14:54:24 -0500

    load data into struct

--- labs/lab5/set8problem3.c
+++ labs/lab5/set8problem3.c
@@ -1,16 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define STUDENTNUM 5
-#define NAMELIMIT 25
+#define LIMIT 25
+#define MAXSTRING 1000
 
 struct Student{
-    int studentId;
-    int score;
-    char name[NAMELIMIT];
+    char studentId[LIMIT];
+    char score[LIMIT];
+    char name[LIMIT];
 };
 
 int main() {
    struct Student students[STUDENTNUM];
+   FILE* fp;
+   char* filename = "student.csv";
+   double topScore = 0.00;
+   int topStudent = 0;
+   char studentName[LIMIT];
+   char string[MAXSTRING];
+   char *token;
+   int count = 0;
+
+   //open file
+   fp = fopen(filename, "r");
+
+   //get first line to ignore
+   fgets(string, MAXSTRING, fp);
+
+   //load file into struct
+   while(feof(fp) != true){
+    fgets(string, MAXSTRING, fp);
+    token = strtok(string, ",");
+    strncpy(students[count].name, token, LIMIT);
+    token = strtok(NULL, ",");
+    strncpy(students[count].studentId, token, LIMIT);
+    token = strtok(NULL, ",");
+    strncpy(students[count].score, token, LIMIT);
+    count++;
+   }
+
+   for(int i=0; i<STUDENTNUM; i++){
+    if(topScore < atof(students[i].score)){
+        topStudent = i;
+    }
+   }
+
+   printf("Top scorer: %s %s", students[topStudent].name, students[topStudent].studentId);
    
    return 0;
 }
