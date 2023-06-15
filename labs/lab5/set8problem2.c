827ed72c46d24f57bfbf3a2f5f8d28a0cf5d9086
sfulle14 <steven.fuller96@gmail.com>
2023-06-15 12:05:33 -0500

    start set8problem1

--- labs/lab5/set8problem2.c
+++ labs/lab5/set8problem2.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
-struct Length{
-    int feet;
-    int inches;
-};
 
 int main() {
-    struct Length L1;
-    struct Length L2;
 
-    //get Length 1
-    printf("Enter Length 1 feet and inches: ");
-    scanf("%d", L1.feet);
-    scanf("%d", L1.inches);
-
-    //get Length 2
-    printf("Enter Length 2 feet and inches: ");
-    scanf("%d", L2.feet);
-    scanf("%d", L2.inches);
-   
-   return 0;
+    return 0;
 }
