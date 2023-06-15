f75286bd4d7ba84c93c940c1a87fdb89d0fc6ff4
sfulle14 <steven.fuller96@gmail.com>
2023-06-14 19:42:54 -0500

    start lab 6

--- labs/lab6/main.c
+++ labs/lab6/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+struct Struct_Employee_Info{
+   char firstName[100];
+   char lastName[100];
+   char zipCode[6];
+   char department[100];
+   char salary[10];
+};
+
+
+
 int main() {
+   struct Struct_Employee_Info emp[100];
+   char comparisonOperator[2];
    
-   
+   return 0;
+}
+
+int SearchByName(struct Struct_Employee_Info employeeStructure, char firstName[100], char lastName[100]){
+
+   return 0;
+}
+
+
+int SearchByZipCode(struct Struct_Employee_Info employeeStructure, char zipCode[6]){
+
+   return 0;
+}
+
+int SearchBySalary(struct Struct_Employee_Info employeeStructure, char salary[10], char comparisonOperator[2]){
+
+
    return 0;
 }
