f71699c16aeea1baf02b662ec6c8140b09455267
sfulle14 <steven.fuller96@gmail.com>
2023-06-15 11:33:57 -0500

    start set7project3

--- labs/lab4/set7problem3.c
+++ labs/lab4/set7problem3.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
+#define LIMIT 100
 
 int main() {
+   FILE *filePtr;
+   char* filename = "userdata.txt";
+   char username[LIMIT];
+   char password[LIMIT];
+   char fileUsername[LIMIT];
+   char filePassword[LIMIT];
+   char fileAmount[LIMIT];
+   char* string;
+
+   filePtr = fopen(filename, "r");
+
+
+   printf("Username: ");
+   scanf("%s", username);
+   printf("\n");
+   printf("Password: ");
+   scanf("%s", password);
+   printf("\n");
    
+   while(fgets(string, LIMIT, filePtr) != NULL){
+      sscanf(string, "%s %s %s", fileUsername, filePassword, fileAmount);
+      if(strcmp(username, fileUsername) && strcmp(password, filePassword)){
+         printf("The amount is: %s\n", fileAmount);
+      }
+      else{
+         printf("username/password mismatch!!!\n");
+      }
+   }
+
    
    return 0;
 }
