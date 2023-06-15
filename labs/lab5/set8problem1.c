5aa8d5ec877acd78e72e94d7a29907ebe5518f5c
sfulle14 <steven.fuller96@gmail.com>
2023-06-15 12:27:03 -0500

    set8problem1 working

--- labs/lab5/set8problem1.c
+++ labs/lab5/set8problem1.c
@@ -6,18 +6,48 @@ struct Length{
 };
 
 int main() {
-    struct Length L1;
-    struct Length L2;
+   struct Length L1;
+   struct Length L2;
+   struct Length result;
 
-    //get Length 1
-    printf("Enter Length 1 feet and inches: ");
-    scanf("%d", &L1.feet);
-    scanf("%d", &L1.inches);
+   //get Length 1
+   printf("Enter Length 1 feet and inches: ");
+   scanf("%d", &L1.feet);
+   scanf("%d", &L1.inches);
 
-    //get Length 2
-    printf("Enter Length 2 feet and inches: ");
-    scanf("%d", &L2.feet);
-    scanf("%d", &L2.inches);
+   //get Length 2
+   printf("Enter Length 2 feet and inches: ");
+   scanf("%d", &L2.feet);
+   scanf("%d", &L2.inches);
    
+   //sum
+   result.feet = L1.feet + L2.feet;
+   result.inches = L1.inches + L2.inches;
+
+   if(result.inches>=12){
+      result.feet += result.inches/12;
+      result.inches = result.inches%12;
+   }
+
+   printf("Sum:\n");
+   printf("Result feet: %d\n", result.feet);
+   printf("Result inches: %d\n\n", result.inches);
+
+   //difference
+   result.feet = L1.feet - L2.feet;
+   result.inches = L1.inches - L2.inches;
+
+   if(result.inches < 0){
+      result.feet -= 1;
+      result.inches = result.inches + 12;
+   }
+
+   printf("Difference:\n");
+   printf("Result feet: %d\n", result.feet);
+   printf("Result inches: %d\n\n", result.inches);
+
+
+
+
    return 0;
 }
