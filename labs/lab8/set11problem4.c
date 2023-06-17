e40cd78541b8334353c4b522d8df5b9c22bc6c8f
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 20:35:21 -0500

    set11problem4 working

--- labs/lab8/set11problem4.c
+++ labs/lab8/set11problem4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
    FILE* fp;
@@ -6,23 +7,48 @@ int main(int argc, char* argv[]) {
    int numStudents;
    int num;
    char c;
-   char* str;
+   float* str;
+   int count = 0;
+   float tmp;
 
    if(argc == 2){
       filename = argv[1];
 
-      fp = open(filename, "r");
+      //open file
+      fp = fopen(filename, "r");
 
-      num = fgetc(fp);
+      //get first line of file
+      c = fgetc(fp);
+      num = atoi(&c);
 
-      str = (char*)malloc(num * sizeof(char));
+      //dynamically allocate space based on first line of file
+      str = (float*)malloc(num * sizeof(float));
 
-      while((c = fgetc(fp)) != EOF){
-         
+      //read in scores into array
+      while(fscanf(fp,"%f", &tmp) != EOF){
+         str[count] = tmp;
+         count++;
       }
+
+      //sort list of scores
+      for(int i=0; i<num; i++){
+         for(int j=i; j<num; j++){
+            if(str[i]>str[j]){
+               tmp=str[i];
+               str[i] = str[j];
+               str[j] = tmp;
+            }
+         }
+      }
+
+      //print out sorted list of scores
+      for(int i=0; i<num; i++){
+         printf("%f, ", str[i]);
+      }
+      
    }
    
-
-   
+   //close file
+   fclose(fp);
    return 0;
 }
