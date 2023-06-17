4380b5838738ee671614df43cf0d43c99e8fc222
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 19:24:34 -0500

    set10problem3 working

--- labs/lab7/set10problem3.c
+++ labs/lab7/set10problem3.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
    char s[10] = "abcde";
    char* cptr;
-   
-   // WRITE YOUR CODE HERE
+   int len = strlen(s);
+
+   // prints the characters in a string in reverse order.
+   for(int i=len; i>=0; i--){
+      cptr = &s[i];
+      printf("%c", *cptr);
+   }
    
    return 0;
 }
