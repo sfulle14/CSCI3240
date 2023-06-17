7eabd81f2c18759944831e28bab06e35ed969265
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 20:07:09 -0500

    start set11problem4

--- labs/lab8/set11problem4.c
+++ labs/lab8/set11problem4.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+   FILE* fp;
+   char* filename;
+   int numStudents;
+   int num;
+   char c;
+   char* str;
+
+   if(argc == 2){
+      filename = argv[1];
+
+      fp = open(filename, "r");
+
+      num = fgetc(fp);
+
+      str = (char*)malloc(num * sizeof(char));
+
+      while((c = fgetc(fp)) != EOF){
+         
+      }
+   }
    
+
    
    return 0;
 }
