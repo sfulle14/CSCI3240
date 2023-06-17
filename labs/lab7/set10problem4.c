e38a1e4e1a68b314240ec798f914811dc2346c8a
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 19:30:08 -0500

    set10problem4 working

--- labs/lab7/set10problem4.c
+++ labs/lab7/set10problem4.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main() {
+   int arr[]= {1,5,4,7,2};
+   int* iptr;
+   int smallest=arr[0];
+   int largest=0;
+
+   for(int i = 0; i<5; i++){
+      iptr = &arr[i];
+      if(*(iptr)>largest){
+         largest = *(iptr);
+      }
+      else if(*(iptr)< smallest){
+         smallest = *(iptr);
+      }
+   }
    
-   
+   printf("Largest: %d\n", largest);
+   printf("Smallest: %d\n", smallest);
+
    return 0;
 }
