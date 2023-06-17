74faf23d349a67696afc324e0a301d38970ecbdc
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 19:56:22 -0500

    set11problem3 working

--- labs/lab8/set11problem3.c
+++ labs/lab8/set11problem3.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+/*
+int * p1 (void) {
+  int x= 10;
+return (&x);
+}
+
+int * p2 (void) {
+  int * px;
+  *px= 10;
+  return px;
+}
+*/
+int *p3 (void) {
+   int *px;
+   px = (int *) malloc (sizeof(int));
+   *px= 10;
+   return px;
+}
 
 int main() {
-   
-   
+   int* n;
+   n =p3();
    return 0;
 }
+
+
+//D. P1 and P2
