d6c8d1d062fae9e038494e9c970c28e25197a91d
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 19:51:30 -0500

    set11problem1&2 working

--- labs/lab8/set11problem1.c
+++ labs/lab8/set11problem1.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-   
+   int *p;
+   p = (int*) calloc(3,sizeof(int));
+
+   printf("Enter first number\n");
+   scanf("%d",p);
+
+   printf("Enter second number\n");
+   scanf("%d",p+2);
+
+   printf("%d%d",*p,*(p+2));
+
+   free(p);
    
    return 0;
 }
+
+// A. 27
--- labs/lab8/set11problem2.c
+++ labs/lab8/set11problem2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
-   
+   //Which of the following header files must n
+   //ecessarily be included to use dynamic memory 
+   //allocation functions?
+
+   //A. stdlib.h
    
    return 0;
 }
