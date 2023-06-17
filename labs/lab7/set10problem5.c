b41825bf69203baecee1c8a470fa45d4a19dd674
sfulle14 <steven.fuller96@gmail.com>
2023-06-16 19:43:21 -0500

    set10problem5 working

--- labs/lab7/set10problem5.c
+++ labs/lab7/set10problem5.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-   
+   char str[] = "Hello world";
+   char* cptr;
+   int vowels=0;
+   int consonants = 0;
+
+   int len = strlen(str);
+
+   for(int i=0; i<len; i++){
+      cptr=&str[i];
+      if(*(cptr)=='a' || *(cptr)=='A' || *(cptr)=='e' || *(cptr)=='E' || *(cptr)=='i' || *(cptr)=='I' || *(cptr)=='o' || *(cptr)=='O' || *(cptr)=='u' || *(cptr)=='U'){
+         vowels++;
+      }
+      else if(*cptr != ' ' && *cptr != ',' && *cptr != '.'){
+         consonants++;
+      }
+   }
+
+   printf("Vowels: %d\n", vowels);
+   printf("Consonants: %d\n", consonants);
+
    
    return 0;
 }
