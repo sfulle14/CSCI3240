766b0dcd43a57a04a797a6747b1ff963372a8346
sfulle14 <steven.fuller96@gmail.com>
2023-06-22 15:57:18 -0500

    lab11 problem3 working

--- labs/lab11/problem3.c
+++ labs/lab11/problem3.c
@@ -1,34 +1,72 @@
+//
+//Implement a parallel bubble sort algorithm in C using pthreads.
+//
 #include <pthread.h>
 #include <stdio.h>
 
+#define ARRSIZE 5
+
 //gloabl variables
 pthread_mutex_t mutex;
-int counter = 0;
+int arr[ARRSIZE]= {3,1,4,5,2};
+int tmp;
+int max_threads = (ARRSIZE + 1) / 2;
+
 
 //thread function
 void *my_thread_function(void *arg)
 {
-   for (int i = 0; i < 1000000; i++) {
-      pthread_mutex_lock(&mutex);
-      counter++;
-      pthread_mutex_unlock(&mutex);
+   int index = tmp;
+   tmp = tmp+2;
+   int holder;
+
+   if((index+1<ARRSIZE) && (arr[index] > arr[index+1])){
+      //swap
+      holder = arr[index];
+      arr[index] = arr[index+1];
+      arr[index+1] = holder;
    }
-   return; 
+   return NULL; 
 }
 
 
 int main() {
-   pthread_t my_thread1, my_thread2;
+   
+   pthread_t my_thread[max_threads];
 
    pthread_mutex_init(&mutex, NULL);
 
-   pthread_create(&my_thread1, NULL, my_thread_function, NULL);
-   pthread_create(&my_thread2, NULL, my_thread_function, NULL);
+   for(int i=1; i<=ARRSIZE; i++){
+      if(i%2==1){
+         tmp=0;
+         //creating thread
+         for(int j=0; j<max_threads; j++){
+            pthread_create(&my_thread[j], NULL, my_thread_function, NULL);
+         }
+         //waiting for thread to finish
+         for(int j=0; j<max_threads; j++){
+            pthread_join(my_thread[j], NULL);
+         }
+      }
+      else{
+         tmp=1;
+         //creating thread
+         for(int j=0; j<max_threads; j++){
+            pthread_create(&my_thread[j], NULL, my_thread_function, NULL);
+         }
+         //waiting for thread to end
+         for(int j=0; j<max_threads; j++){
+            pthread_join(my_thread[j], NULL);
+         }
+      }
+   }
 
-   pthread_join(my_thread1, NULL);
-   pthread_join(my_thread2, NULL);
 
-   printf("Final value of counter: %d\n", counter);
+   //Print Sorted list
+   printf("Sorted array: ");
+   for(int i=0; i<ARRSIZE; i++){
+      printf("%d ", arr[i]);
+   }
 
    pthread_mutex_destroy(&mutex);
    return 0;
