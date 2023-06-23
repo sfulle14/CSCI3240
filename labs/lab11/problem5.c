55260ba364ede97ce32f5432499d642600a93d6e
sfulle14 <steven.fuller96@gmail.com>
2023-06-23 08:45:22 -0500

    started problem5

--- labs/lab11/problem5.c
+++ labs/lab11/problem5.c
@@ -1,35 +1,92 @@
 #include <pthread.h>
 #include <stdio.h>
 
+#define LENGTH 5
+#define THREADS 2
+
 //gloabl variables
-pthread_mutex_t mutex;
-int counter = 0;
+typedef struct qsort_starter{
+   int *arr;
+   int low;
+   int high;
+   int depthOfThread;
+} quicksort_parameters;
+
+int arr[LENGTH] = {3,1,4,5,2};
+
+void swap(int *a, int*b);
+int partition(int *arr, int low, int high, int pivot);
+void c_quickSort(int *arr, int low, int high, int depthOfThread);
+void quickSort(int *arr, int low, int high);
 
 //thread function
 void *my_thread_function(void *arg)
 {
-   for (int i = 0; i < 1000000; i++) {
-      pthread_mutex_lock(&mutex);
-      counter++;
-      pthread_mutex_unlock(&mutex);
-   }
-   return; 
+   quicksort_parameters* parameters = arg;
+   c_quickSort(parameters -> arr, parameters->low, parameters->high, parameters->depthOfThread);
+   return NULL; 
 }
 
 
 int main() {
-   pthread_t my_thread1, my_thread2;
+   int depthOfThread = THREADS;
 
-   pthread_mutex_init(&mutex, NULL);
+   c_quickSort(arr, 0, LENGTH-1, depthOfThread);
 
-   pthread_create(&my_thread1, NULL, my_thread_function, NULL);
-   pthread_create(&my_thread2, NULL, my_thread_function, NULL);
+   printf("Sorted arr: ");
+   for(int i=0; i<LENGTH; i++){
+      printf("%d", arr[i]);
+   }
 
-   pthread_join(my_thread1, NULL);
-   pthread_join(my_thread2, NULL);
-
-   printf("Final value of counter: %d\n", counter);
-
-   pthread_mutex_destroy(&mutex);
    return 0;
+}
+
+void swap(int *a, int*b){
+   int t= *a;
+   *a = *b;
+   *b = t;
+}
+
+int partition(int *arr, int low, int high, int pivot){
+   int pivotValue = arr[pivot];
+   swap(&arr[pivot], &arr[high]);
+   int s = low;
+
+   for(int i=low; i<high; i++){
+      if(arr[i] <= pivotValue){
+         swap(&arr[i], &arr[s]);
+      }
+   }
+   swap(&arr[s], &arr[high]);
+   return s;
+}
+
+void c_quickSort(int *arr, int low, int high, int depthOfThread){
+   if(low<high){
+      int pivotPos = low + (high-low) / 2;
+      pivotPos = partition(arr, low, high, pivotPos);
+      pthread_t thread;
+
+      if(depthOfThread > 0){
+         quicksort_parameters thread_param = {arr, low, pivotPos-1, depthOfThread};
+         int result;
+         result = pthread_create(&thread, NULL, my_thread_function, &thread_param);
+         c_quickSort(arr, pivotPos+1, high, depthOfThread);
+
+         pthread_join(thread, NULL);
+      }
+      else{
+         quickSort(arr, low, pivotPos-1);
+         quickSort(arr, pivotPos+1, high);
+      }
+   }
+}
+
+void quickSort(int *arr, int low, int high){
+   if(low<high){
+      int pivotPos = low + (high-low) / 2;
+      pivotPos = partition(arr, low, high, pivotPos);
+      quickSort(arr, low, pivotPos-1);
+      quickSort(arr, pivotPos+1, high);
+   }
 }
